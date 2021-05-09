5673fd9ac9326d1b5801eeb55ca2549ab3e2467b
Julius Isken <>
2021-05-09 10:51:31 +0200

    rewrote driver.cpp

--- Small_GEMMs/driver.cpp
+++ Small_GEMMs/driver.cpp
@@ -1,5 +1,7 @@
 #include "gemm_ref.hpp"
+#include <chrono>
 #include <iostream>
+#include <numeric>
 #include <string>
 
 void printMatr(std::string name, float const *i_x, unsigned int i_m,
@@ -16,27 +18,40 @@ void printMatr(std::string name, float const *i_x, unsigned int i_m,
 }
 
 int main(int argc, char const *argv[]) {
-  unsigned int l_lambda = 2;
+  unsigned int l_m = 2;
+  unsigned int l_n = 2;
+  unsigned int l_k = 2;
 
-  float *l_a = new float[l_lambda * l_lambda];
-  float *l_b = new float[l_lambda * l_lambda];
-  float *l_c = new float[l_lambda * l_lambda];
+  // To simplify things we assumed that the leading dimension was equal to the
+  // number of columns.
+  unsigned int l_lda = l_m;
+  unsigned int l_ldb = l_k;
+  unsigned int l_ldc = l_m;
 
-  for (unsigned int i = 0; i < l_lambda; i++) {
-    for (unsigned int j = 0; j < l_lambda; j++) {
-      unsigned int pos = j + i * l_lambda;
-      l_a[pos] = pos;
-      l_b[pos] = pos;
-      l_c[pos] = pos;
-    }
-  }
-  printMatr("A", l_a, l_lambda, l_lambda, l_lambda);
-  printMatr("B", l_b, l_lambda, l_lambda, l_lambda);
-  printMatr("C", l_c, l_lambda, l_lambda, l_lambda);
+  std::vector<float> l_a(l_m * l_k);
+  std::vector<float> l_b(l_n * l_k);
+  std::vector<float> l_c(l_m * l_n);
 
-  gemm_ref(l_a, l_b, l_c, l_lambda, l_lambda, l_lambda, l_lambda, l_lambda,
-           l_lambda);
+  std::iota(l_a.begin(), l_a.end(), 0);
+  std::iota(l_b.begin(), l_b.end(), 0);
+  std::fill(l_c.begin(), l_c.end(), 0);
 
-  printMatr("Result", l_c, l_lambda, l_lambda, l_lambda);
+  printMatr("A", l_a.data(), l_m, l_k, l_lda);
+  printMatr("B", l_b.data(), l_n, l_k, l_ldb);
+  printMatr("C", l_c.data(), l_m, l_n, l_ldc);
+
+  using namespace std::chrono;
+
+  auto start = system_clock::now();
+
+  gemm_ref(l_a.data(), l_b.data(), l_c.data(), l_m, l_n, l_k, l_lda, l_ldb,
+           l_ldc);
+
+  auto end = system_clock::now();
+
+  double seconds = duration<double>(end - start).count();
+
+  std::cout << "time needed was:" << seconds << '\n';
+  printMatr("Result", l_c.data(), l_m, l_n, l_ldc);
   return 0;
 }
