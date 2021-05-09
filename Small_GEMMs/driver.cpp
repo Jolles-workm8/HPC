7a848dd9cb9b68af577a5aec9ab4e23dfc1a4553
Julius Isken <>
2021-05-09 11:58:41 +0200

    lambda

--- Small_GEMMs/driver.cpp
+++ Small_GEMMs/driver.cpp
@@ -24,49 +24,57 @@ double measure_GFLOPs(double seconds, unsigned int l_m, unsigned int l_n,
 }
 
 int main(int argc, char const *argv[]) {
-  unsigned int l_m = 32;
-  unsigned int l_n = 32;
-  unsigned int l_k = 32;
 
-  // To simplify things we assumed that the leading dimension was equal to the
-  // number of columns.
-  unsigned int l_lda = l_m;
-  unsigned int l_ldb = l_k;
-  unsigned int l_ldc = l_m;
+  std::vector<unsigned int> l_lambda = {4, 8, 12, 16, 24, 32, 48, 64};
 
-  std::vector<float> l_a(l_m * l_k);
-  std::vector<float> l_b(l_n * l_k);
-  std::vector<float> l_c(l_m * l_n);
+  for (size_t i = 0; i < l_lambda.size(); i++) {
 
-  // to increase use iota
-  std::fill(l_a.begin(), l_a.end(), 1);
-  std::fill(l_b.begin(), l_b.end(), 1);
-  std::fill(l_c.begin(), l_c.end(), 0);
+    unsigned int l_m = l_lambda[i];
+    unsigned int l_n = l_lambda[i];
+    unsigned int l_k = l_lambda[i];
 
-  // printMatr("A", l_a.data(), l_m, l_k, l_lda);
-  // printMatr("B", l_b.data(), l_n, l_k, l_ldb);
-  // printMatr("C", l_c.data(), l_m, l_n, l_ldc);
+    // To simplify things we assumed that the leading dimension was equal to the
+    // number of columns.
+    unsigned int l_lda = l_m;
+    unsigned int l_ldb = l_k;
+    unsigned int l_ldc = l_m;
 
-  unsigned int l_count = 5000;
+    std::vector<float> l_a(l_m * l_k);
+    std::vector<float> l_b(l_n * l_k);
+    std::vector<float> l_c(l_m * l_n);
 
-  using namespace std::chrono;
+    // to increase use iota
+    std::fill(l_a.begin(), l_a.end(), 1);
+    std::fill(l_b.begin(), l_b.end(), 1);
+    std::fill(l_c.begin(), l_c.end(), 0);
 
-  auto start = system_clock::now();
+    // printMatr("A", l_a.data(), l_m, l_k, l_lda);
+    // printMatr("B", l_b.data(), l_n, l_k, l_ldb);
+    // printMatr("C", l_c.data(), l_m, l_n, l_ldc);
 
-  for (size_t i = 0; i < l_count; i++) {
-    gemm_ref(l_a.data(), l_b.data(), l_c.data(), l_m, l_n, l_k, l_lda, l_ldb,
-             l_ldc);
+    unsigned int l_count = 10000;
+
+    using namespace std::chrono;
+
+    auto start = system_clock::now();
+
+    for (size_t i = 0; i < l_count; i++) {
+      gemm_ref(l_a.data(), l_b.data(), l_c.data(), l_m, l_n, l_k, l_lda, l_ldb,
+               l_ldc);
+    }
+
+    auto end = system_clock::now();
+
+    double seconds = duration<double>(end - start).count();
+
+    // printMatr("Result", l_c.data(), l_m, l_n, l_ldc);
+
+    std::cout << "Lambda:" << l_lambda[i] << '\n';
+    std::cout << "Time:" << seconds << '\n';
+    std::cout << "GFLOP's: " << measure_GFLOPs(seconds, l_m, l_k, l_n, l_count)
+              << '\n'
+              << '\n';
   }
-
-  auto end = system_clock::now();
-
-  double seconds = duration<double>(end - start).count();
-
-  // printMatr("Result", l_c.data(), l_m, l_n, l_ldc);
-
-  std::cout << "Time:" << seconds << '\n';
-  std::cout << "GFLOP's: " << measure_GFLOPs(seconds, l_m, l_k, l_n, l_count)
-            << '\n';
 
   return 0;
 }
