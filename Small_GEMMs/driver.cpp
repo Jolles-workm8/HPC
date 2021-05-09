11677af350a05025386b261a1a365ab648ccb69f
MaxEngelWeida <maxengelweida@gmail.com>
2021-05-09 13:12:00 +0200

    work on driver.cpp

--- Small_GEMMs/driver.cpp
+++ Small_GEMMs/driver.cpp
@@ -28,6 +28,7 @@ int main(int argc, char const *argv[]) {
 
   std::vector<unsigned int> l_lambda = {4, 8, 12, 16, 24, 32, 48, 64};
 
+  //Performance test of gemm_reff with diffrent matrix sizes
   for (size_t i = 0; i < l_lambda.size(); i++) {
 
     unsigned int l_m = l_lambda[i];
@@ -49,11 +50,8 @@ int main(int argc, char const *argv[]) {
     std::fill(l_b.begin(), l_b.end(), 1);
     std::fill(l_c.begin(), l_c.end(), 0);
 
-    // printMatr("A", l_a.data(), l_m, l_k, l_lda);
-    // printMatr("B", l_b.data(), l_n, l_k, l_ldb);
-    // printMatr("C", l_c.data(), l_m, l_n, l_ldc);
 
-    unsigned int l_count = 10000;
+    unsigned int l_count = 5000;
 
     using namespace std::chrono;
 
@@ -68,7 +66,6 @@ int main(int argc, char const *argv[]) {
 
     double seconds = duration<double>(end - start).count();
 
-    // printMatr("Result", l_c.data(), l_m, l_n, l_ldc);
 
     std::cout << "Lambda:" << l_lambda[i] << '\n';
     std::cout << "Time:" << seconds << '\n';
@@ -76,6 +73,65 @@ int main(int argc, char const *argv[]) {
               << '\n'
               << '\n';
   }
+
+
+  //comparison gemm_compiler_mnk and gemm_compiler_nkm
+  using namespace std::chrono;
+  unsigned int l_count = 50000;
+
+  std::vector<float> l_a(32 * 32);
+  std::vector<float> l_b(32 * 32);
+  std::vector<float> l_c(32 * 32);
+
+  // to increase use iota
+  std::fill(l_a.begin(), l_a.end(), 1);
+  std::fill(l_b.begin(), l_b.end(), 1);
+  std::fill(l_c.begin(), l_c.end(), 0);
+
+
+
+  auto start = system_clock::now();
+
+  for (size_t i = 0; i < l_count; i++) {
+    gemm_compiler_32_32_32_32_32_32_mnk(l_a.data(), l_b.data(), l_c.data());
+  }
+
+  auto end = system_clock::now();
+  double seconds = duration<double>(end - start).count();
+
+
+  std::cout << "gemm_compiler_mnk: " << measure_GFLOPs(seconds, 32, 32, 32, l_count)
+            << "\t GFLOP's"
+            << '\n'
+            << '\n';
+
+
+  // to increase use iota
+  std::fill(l_a.begin(), l_a.end(), 1);
+  std::fill(l_b.begin(), l_b.end(), 1);
+  std::fill(l_c.begin(), l_c.end(), 0);
+
+
+
+  start = system_clock::now();
+
+  for (size_t i = 0; i < l_count; i++) {
+    gemm_compiler_32_32_32_32_32_32_nkm(l_a.data(), l_b.data(), l_c.data());
+  }
+
+  end = system_clock::now();
+  seconds = duration<double>(end - start).count();
+
+
+  std::cout << "gemm_compiler_nkm: " << measure_GFLOPs(seconds, 32, 32, 32, l_count)
+            << "\t GFLOP's"
+            << '\n'
+            << '\n';
+
+
+
+
+
 
   return 0;
 }
