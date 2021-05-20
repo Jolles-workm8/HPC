7740d5cd5cc88284fa85bb509b6368dfc8dde29f
MaxEngelWeida <maxengelweida@gmail.com>
2021-05-20 23:19:08 +0200

    work on task 5.3

---
+++ Assembly_Code/copy.c
@@ -0,0 +1,8 @@
+#include <cstdint>
+
+
+void copy_c(uint32_t * l_a, uint64_t *l_b){
+  for(int i; i<7; i++){
+      l_b[i] = (uint64_t)l_a[i];
+  }
+}
---
+++ Assembly_Code/copy.s
@@ -0,0 +1,22 @@
+
+
+      .text
+      .align 4
+      .type   copy_asm, %function
+      .global copy_asm
+copy_asm:
+
+      
+      ldp w2, w3, [x0]
+      stp x2, x3. [x1]
+      ldp w2, w3, [x0, #8]
+      stp x2, x3, [x1, #16]
+      ldp w2, w3, [x0, #16]
+      stp x2, x3, [x1, #32]
+      ldr w2,     [x0, #24]
+      stp x2      [x1, #48]
+
+
+
+      ret
+      .size copy_asm, (. - copy_asm)
