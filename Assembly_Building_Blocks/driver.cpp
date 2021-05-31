e01ba2f5b66973a65db43ae3546adb3670bda276
julius Isken <julius@ip-172-31-8-41.us-east-2.compute.internal>
2021-05-31 15:36:46 +0000

    driver w time peak and gflop measruement

--- Assembly_Building_Blocks/driver.cpp
+++ Assembly_Building_Blocks/driver.cpp
@@ -1,34 +1,80 @@
 #include <iostream>
+#include <chrono>
+#include <vector>
+#include <cstdint>
+#include <limits>
 
+
+
+double measure_GFLOPs(double seconds, unsigned int l_m, unsigned int l_n,
+                      unsigned int l_k, unsigned int l_count) {
+
+  return (double)(2 * l_k * l_m * l_n * (double)l_count) / (seconds * 1000000000);
+}
 
 extern "C" {
-    void gemm_asm_asimd_16_4_4(uint32_t const * i_a,
-                 uint32_t const    * i_b,
-                 uint32_t          * o_c);
+    void gemm_asm_asimd_16_4_4(float const * i_a,
+                 float const    * i_b,
+                 float          * o_c);
 }
 
 int main(){
-    uint32_t * l_a = new uint32_t[64];
-    uint32_t * l_b = new uint32_t[16];
-    uint32_t * l_c = new uint32_t[64];
+    
+    float * l_a = new float[64];
+    float * l_b = new float[16];
+    float * l_c = new float[64];
 
+    unsigned int l_count = 100000000;
+
+    //init the matrices
     for(size_t i=0; i<64; i++){
-        l_a[i]=i+1;
-        l_c[i]=0;
+        l_a[i]=(float)i;
+        l_c[i]=0.0f;
     }
 
     for(size_t i=0; i<16; i++){
-        l_b[i]=i+1;
+        if(i == 0 || i == 5 || i == 10 || i ==15){
+            l_b[i]=1.0f;
+            }
+        else{
+            l_b[i]=0.0f;
+        }
     }
+    
+    using namespace std::chrono;
 
-    gemm_asm_asimd_16_4_4(l_a, l_b, l_c);
+    auto start = system_clock::now();
+    //execute the kernel l_count times
+    for(size_t i =0; i< l_count; i++){
+        gemm_asm_asimd_16_4_4(l_a, l_b, l_c);
+    }
+    
+    auto end = system_clock::now();
+
+
+    double seconds = duration<double>(end - start).count();
+
+    double gflops = measure_GFLOPs(seconds, 16, 4, 4, l_count);
+
+    std::cout << "Time: " << seconds << '\n'
+        << "Executions: " << l_count << '\n'
+        << "GFLOPs: " << gflops << '\n'
+        << "Peak %: " << gflops/40.0 << '\n';
+    
+    std::cout << "unsigned float limits: " << std::numeric_limits<float>::max() << '\n';
 
     for(size_t i=0; i<64; i++){
         if(i%4 ==0 ){
-            std::cout << "\n";
+            std::cout << "\n" << " | ";
         }
-        std::cout<< " | " << l_c[i] << " | ";       
-        
+        std::cout<< l_c[i] << " | ";       
     }
+    std::cout<< std::endl << std::endl;
 
+    delete[] l_a;
+    delete[] l_b;
+    delete[] l_c;
+
+
+    return EXIT_SUCCESS;
 }
